e07d29e62d05dc148e1858a24a8062596bdfe6ad
Devin Ryan <devin.ryan514@gmail.com>
2018-06-25 03:38:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -15,9 +15,27 @@ function frameIt(){
   return;
 }
 
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if(key.which === 71){
+      alert('G was pressed');
+    }
+    return;
+  });
+  return;
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
 getIt();
-
+frameIt();
+pressIt();
+submitIt();
 });
