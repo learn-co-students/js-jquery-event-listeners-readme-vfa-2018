a0e47c1e6728956ce7311dd6f9371d5e4d185bcc
Devin Ryan <devin.ryan514@gmail.com>
2018-06-25 03:28:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
 //define functions here
+function getIt(){
+ $('p').on('click', function(){
+    alert("Hey!");
+    return;
+ });
+ return;
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+    return;
+  });
+  return;
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
