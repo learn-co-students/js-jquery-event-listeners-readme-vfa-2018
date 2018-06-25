fc667f5ed3b6d758675537494d37689f741db958
Hannah Hund <hundxhc@gmail.com>
2018-06-25 18:42:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+    return;
+  });
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img:first').on('load', function(){
+    $('img:first').addClass('tasty');
+    return;
+  });
+}
 
-// call functions here
+function pressIt() {
+  $("#typing").on('keydown', function(e){
+    if (e.which === 71) {
+      alert('You pressed the G key!');
+      return;
+    }
+  });
+}
 
-});
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
+
+ 
+ $(document).ready(function(){
+ 
+ // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+ 
+ });
