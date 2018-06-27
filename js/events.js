2beeae9112b2164dde72f95c58f95323ceaeedc3
Grayson Wise <graysonwise3@gmail.com>
2018-06-27 16:39:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,38 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+    return;
+  });
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert("You pressed G");
+    }
+    return;
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
