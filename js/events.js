e1e980a977df61d395cf9c464487dc2053b1e14a
Apurva Kasam <apurva.kasam@gmail.com>
2018-06-28 20:28:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,16 @@
 $(document).ready(function(){
 
 // call functions here
+$('p').on("eventname", getIt())
+
+$('img').on('load', frameIt())
 
 });
+
+function getIt() {
+  return "Hey!"
+}
+
+function frameIt() {
+  $('img').append(".tasty")
+}
