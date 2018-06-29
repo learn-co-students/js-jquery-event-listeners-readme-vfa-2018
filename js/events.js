abe5ed644d37a4d22c10ed89e28f6606fd387b9b
Apurva Kasam <apurva.kasam@gmail.com>
2018-06-29 15:03:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,18 +1,33 @@
 //define functions here
 
-$(document).ready(function(){
-
-// call functions here
-$('p').on("eventname", getIt())
-
-$('img').on('load', frameIt())
-
-});
-
 function getIt() {
-  return "Hey!"
+  alert ("Hey!");
 }
 
 function frameIt() {
-  $('img').append(".tasty")
+  $("img").addClass('tasty');
 }
+
+function pressIt() {
+  $(document).on('keydown', function(key){
+    if(key.which == 71) {
+      alert('G was pressed');
+    }
+  })
+}
+
+function submitIt() {
+  alert ('Your form is going to be submitted now.');
+}
+
+
+$(document).ready(function(){
+
+// call functions here
+$('p').click(getIt())
+
+$('img').load(frameIt())
+
+
+});
+
