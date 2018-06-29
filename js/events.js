206328853335fd60ff971e18e21dbbe71fd22dc8
Dan Bardach <bardachd@xavier.edu>
2018-06-29 04:43:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,17 @@
-//define functions here
-
-$(document).ready(function(){
-
-// call functions here
-
-});
+function getIt() {
+  $('p') .click("hey!")
+  return;
+}
+function frameIt() {
+  $("img") .addClass("tasty")
+  return;
+}
+function pressIt() {
+  alert('g')
+  return;
+}
+$("form").on("submit")
+function submitIt() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+}}
