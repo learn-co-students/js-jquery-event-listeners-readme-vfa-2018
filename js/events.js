aaed52b08b011e4be796fce50b280fff6cd90f52
Joseph Cochran <joseph.cochran@vanderbilt.edu>
2018-06-29 19:05:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,41 @@
 //define functions here
+$('form').on('submit', function() {
+  if ($( 'input:first' ).val() === 'correct') {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert('you entered the wrong value');
+  return;
+});
+
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!');
+    return;
+  });
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    ('img').addClass('#tasty');
+  });
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key) {
+    if(key.which ==71){
+      alert('g was pressed');
+      return;
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now');
+    return;
+  });
+}
 
 $(document).ready(function(){
 
