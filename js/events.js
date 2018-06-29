6154731483cfba73affddc1c470c27b46cdd7e94
Danielle Bennett <dab621@stern.nyu.edu>
2018-06-29 08:02:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,19 @@
 //define functions here
+getIt = $('p').on("dblclick keypress", function(){
+  alert("Hey!")
+});
 
-$(document).ready(function(){
-
+frameIt = $('.tasty').on('load',
+function(){'add red frame to image'}
+);
+pressIt = $("form").on('keydown', function(key) {
+  if(key.which == 83){
+      alert('G was pressed');
+  }
+});
+submitIt = $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('Your form is going to be submitted now');}
 // call functions here
 
 });
