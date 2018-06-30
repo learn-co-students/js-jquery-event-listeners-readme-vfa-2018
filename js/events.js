101a9bd80c6355cfa459001435e4d094efefc1fe
Gaby Nair <gaby1113@gmail.com>
2018-06-30 20:44:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
+var getIt=$('p').on('click', function () { 
+  alert("Hey!") 
+  return; 
+}) 
+
+var frameIt=$('img').on('load', 
+function() {
+  add('.tasty'); 
+  img.style.bordercolor='red';
+})
 
 $(document).ready(function(){
 
 // call functions here
 
 });
+
+getIt.on('click') 
+
+$('img').on('load', 
+function() {
+  add('.tasty'); 
+  img.style.bordercolor='red';
+})
+
