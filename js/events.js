c379d1a234614b69af7e49f84c39cb42262b5db4
Dan Bardach <bardachd@xavier.edu>
2018-06-30 18:10:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,17 +1,20 @@
 function getIt() {
-  $('p') .click("hey!")
-  return;
+ $('p').on('click', () => {alert('Hey!')})
 }
 function frameIt() {
   $("img") .addClass("tasty")
   return;
 }
 function pressIt() {
-  alert('g')
-  return;
+  $('form').on('keydown', (e) => {
+    if (e.which == 71) {
+      alert('just a g thang')    }
+ })
 }
-$("form").on("submit")
+
+
 function submitIt() {
-  if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
-}}
+  $('form').on('submit', () => {
+    alert("Your form is going to be submitted now.")
+  })
+}
