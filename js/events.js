5abb81406ac31dda443ca207c9a91fae88ff5263
Robert Hwang <robert.t.hwang@gmail.com>
2018-06-30 21:56:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,37 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+
+function getIt() {
+  $('p').on("click", function() {
+    alert('u clicked a paragraph');
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('you have pressed the G key')
+    }
+    return;
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert('your form is going to be submitted now')
+    return;
+  })
+}
