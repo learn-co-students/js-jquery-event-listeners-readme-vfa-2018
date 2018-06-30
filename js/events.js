227ef8aaa2da549a0e30fde85d60ba3ae5cb2f0d
Robert Hwang <robert.t.hwang@gmail.com>
2018-06-30 21:58:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -33,7 +33,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on("submit", function() {
-    alert('your form is going to be submitted now')
+    alert('Your form is going to be submitted now.')
     return;
   })
 }
